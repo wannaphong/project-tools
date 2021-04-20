5252ccdf5c00e21f01f0a6df80a67a5158d3a1da
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-04-20 14:56:42 +0700

    Update util.py

--- gen-code2docx/util.py
+++ gen-code2docx/util.py
@@ -5,9 +5,14 @@ def readcode(file):
         return f.read()
 
 list_filecode = list(glob.glob('./source/*.*'))
-print(list_filecode)
+#print(list_filecode)
 list_code = [readcode(i) for i in list_filecode]
-dict_code = [{'file':os.path.basename(i),'code':j,'num':'2.1.'+str(list_code.index(j)+1)} for i,j in zip(list_filecode, list_code)]
-
+files = """
+""".splitlines()
+#files = [i.strip() for i in files]
+#print(files)
+import html
+dict_code = [{'file':os.path.basename(i),'code':html.escape(j),'num':'2.2.'+str(list_code.index(j)+1)} for i,j in zip(list_filecode, list_code)]# if os.path.basename(i) in files]
+#print(dict_code)
 def get_data():
     return dict_code
